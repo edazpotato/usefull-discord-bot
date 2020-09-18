f6d84e8277cafc1435f73d81287d56bbd9c1204d
Edaz Potato <edazpotato@gmail.com>
2020-09-18 22:04:07 +1200

    Add enviroment variables

--- bot.py
+++ bot.py
@@ -1,13 +1,21 @@
 import discord
-from dsicord.ext import commands
+from discord.ext import commands
 
-class Client(commands.AutoShardedBot):
-    async def on_ready(self):
-        print(f"Logged in to discord as {self.user.name}#{self.user.tag}")
-        print(f"Add me you your server with this url: https://discord.com/oauth2/authorize?client_id={self.user.id}&scope=bot&permissions=2146958847")
+# Load environment variables if they aren't loaded yet
+if not os.getenv("DISCORD_TOKEN"):
+    from dotenv import load_dotenv
+    load_dotenv()
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+class Client(commands.Bot):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+	async def on_ready(self):
+		print(f"Logged in to discord as {self.user.name}#{self.user.tag}")
+		print(f"Add me you your server with this url: https://discord.com/oauth2/authorize?client_id={self.user.id}&scope=bot&permissions=2146958847")
+
+	async def on_message(self, message):
+		print('Message from {0.author}: {0.content}'.format(message))
 
 client = Client()
 client.run(os.getEnv("DISCORD_TOKEN"))
